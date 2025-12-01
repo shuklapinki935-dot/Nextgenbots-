fcc60be7c5c0c6dae0584104fa86ea96248f7126
Badnam465 <kritikumari8665@gmail.com>
2025-12-01 15:30:41 +0530

    Update ping.py

--- SONALI_MUSIC/plugins/tools/ping.py
+++ SONALI_MUSIC/plugins/tools/ping.py
@@ -1,26 +1,26 @@
 from datetime import datetime
-
 from pyrogram import filters
-from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from config import *
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from SONALI_MUSIC import app
 from SONALI_MUSIC.core.call import Sona
 from SONALI_MUSIC.utils import bot_sys_stats
 from SONALI_MUSIC.utils.decorators.language import language
 from SONALI_MUSIC.utils.inline import supp_markup
+from SONALI_MUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+
+
+@app.on_message(filters.command("ping", prefixes=["/", "!"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://files.catbox.moe/plxzb4.mp4",
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await SONALI_MUSIC.ping()
+    pytgping = await Sona.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(

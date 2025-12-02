5de4c0fee70fe8d95e3e8f11ca76b85a3a0e45e9
Coder0 <coder0481@gmail.com>
2025-12-02 12:58:35 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -94,10 +94,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         
         callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             
-            InlineKeyboardButton(
-                text=_["P_B_2"],
-                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
-            ),
+
         
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
     buttons = [
